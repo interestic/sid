88a89293b9146c4d3bfff7d231378e9b656b06b1
do9iigane <dokumegane@interestic.com>
2016-04-27 15:45:36 +0900

    split task notify slack

--- Envoy.blade.php
+++ Envoy.blade.php
@@ -5,8 +5,10 @@
 
 @macro('deploy')
     opening_ceremony
-    clone_env
-    oscillo_setup
+    download_source
+    setup_composer
+    copy_vendor
+    composer_update
 @endmacro
 
 @macro('shared_init')
@@ -18,22 +20,47 @@
     echo {{$env}} deploy started.
 @endtask
 
-@task('clone_env')
+@task('download_source')
     echo clone to env
     cd {{$clone_dir}}
     pwd
     wget --no-check-certificate https://github.com/interestic/oscillo/archive/{{$env}}.tar.gz
     echo download complete
+    tar -xzf {{$env}}.tar.gz && rm -f {{$env}}.tar.gz && mv oscillo-dev/* ./ && rm -rf oscillo-dev
 @endtask
 
-@task('oscillo_setup')
-    echo setup oscillo
+@task('setup_composer')
+    echo setup composer
     cd {{$clone_dir}}
-    tar -xzf {{$env}}.tar.gz && rm -f {{$env}}.tar.gz && mv oscillo-dev/* ./ && rm -rf oscillo-dev
+
     export COMPOSER_HOME=./
     php -r "readfile('https://getcomposer.org/installer');" > composer-setup.php
     php -r "if (hash_file('SHA384', 'composer-setup.php') === '7228c001f88bee97506740ef0888240bd8a760b046ee16db8f4095c0d8d525f2367663f22a46b48d072c816e7fe19959') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
     php composer-setup.php
     php -r "unlink('composer-setup.php');"
+@endtask
+
+@task('copy_vendor')
+    echo copy vendor dir
+    cd {{$clone_dir}}
+
+    cp -rp ../../_shared/vendor ./
+@endtask
+
+@task('composer_install')
+    echo composer install
+    cd {{$clone_dir}}
+
     php composer.phar -v install
 @endtask
+
+@task('composer_update')
+    echo composer update
+    cd {{$clone_dir}}
+
+    php composer.phar -v update
+@endtask
+
+@after
+    @slack('https://hooks.slack.com/services/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', '#app', "$task ran on [$env]")
+@endafter
